679e2b9dd5552d1f5ee908f1f0fa3456c45c12ea
Joelle Vetting <joelle.vetting@bit.nwtc.edu>
2025-11-02 09:56:50 -0600

    Deleted unnecessary comments

---
+++ ZooScenario/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZooScenario
+{
+    /// <summary>
+    /// Contains interaction logic for MainWindow.xaml.
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Encapsulation not yet taught.")]
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Performs the tasks outlined in assignment 1.2.
+        /// </summary>
+        /// <param name="sender">The object that initiated the event.</param>
+        /// <param name="e">The event arguments for the event.</param>
+        private void assignment12Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            // Create an instance of the Zoo class.
+
+            // Set field values of the zoo.
+
+            // Set field values of the ladies' restroom.
+
+            // Set field values of the men's restroom.
+
+            // Set field values of the ticket booth.
+
+            // Set field values of the ticket booth attendant.
+        }
+    }
+}
