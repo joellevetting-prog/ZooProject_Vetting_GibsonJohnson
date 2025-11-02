dd9781c0a7db52a545fa69622215b27d973f7fab
Joelle Vetting <joelle.vetting@bit.nwtc.edu>
2025-11-02 09:44:57 -0600

    Add project files.

---
+++ zooscenario/Business Classes/Booth.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooScenario
+{
+    /// <summary>
+    /// The class used to represent a ticket booth.
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Encapsulation not yet taught.")]
+    public class Booth
+    {
+        /// <summary>
+        /// The booth attendant.
+        /// </summary>
+        public Employee Attendant;
+
+        /// <summary>
+        /// The number of items sold.
+        /// </summary>
+        public int ItemCountSold;
+
+        /// <summary>
+        /// The price of an individual item for sale.
+        /// </summary>
+        public decimal ItemPrice;
+
+        /// <summary>
+        /// The type of item for sale.
+        /// </summary>
+        public string ItemType;
+
+        /// <summary>
+        /// The amount of money currently in the ticket booth.
+        /// </summary>
+        public decimal MoneyBalance;
+    }
+}
---
+++ zooscenario/Business Classes/Zoo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooScenario
+{
+    /// <summary>
+    /// The class used to represent a zoo.
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Encapsulation not yet taught.")]
+    public class Zoo
+    {
+        /// <summary>
+        /// The maximum number of guests the zoo can accommodate at a given time.
+        /// </summary>
+        public int Capacity;
+
+        /// <summary>
+        /// The ladies' restroom.
+        /// </summary>
+        public Restroom LadiesRoom;
+
+        /// <summary>
+        /// The men's restroom.
+        /// </summary>
+        public Restroom MensRoom;
+
+        /// <summary>
+        /// The name of the zoo.
+        /// </summary>
+        public string Name;
+
+        /// <summary>
+        /// The ticket booth.
+        /// </summary>
+        public Booth TicketBooth;
+    }
+}
